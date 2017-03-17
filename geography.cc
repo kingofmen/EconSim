a9b5ca38142fccc7c37f777fab1633c77bdd4d51
kingofmen <rolf.andreassen@gmail.com>
2017-03-16 23:36:22 -0700

    Check for possible production chains

--- geography.cc
+++ geography.cc
@@ -1,7 +1,40 @@
 #include "geography/geography.h"
 
+#include <vector>
+
+#include "industry/proto/industry.pb.h"
+#include "market/goods_utils.h"
+
 namespace geography {
 
-void Field::PossibleProductionChains() const {}
+void Field::PossibleProductionChains(
+    const std::vector<const industry::proto::Production *> &candidates,
+    std::vector<const industry::proto::Production *> *possible) const {
+  for (const auto *candidate : candidates) {
+    if (candidate->land_type() != land_type()) {
+      continue;
+    }
+    bool can_do_all_steps = true;
+    for (const auto& step : candidate->steps()) {
+      bool can_do_step = false;
+      for (const auto& variant : step.variants()) {
+        if (fixed_capital() < variant.fixed_capital()) {
+          continue;
+        }
+        can_do_step = true;
+        break;
+      }
+      if (can_do_step) {
+        continue;
+      }
+      can_do_all_steps = false;
+      break;
+    }
+    if (!can_do_all_steps) {
+      continue;
+    }
+    possible->push_back(candidate);
+  }
+}
 
 } // namespace geography
--- geography.h
+++ geography.h
@@ -5,8 +5,10 @@
 namespace geography {
 
 class Field : public proto::Field {
- public:
-  void PossibleProductionChains() const;
+public:
+  void PossibleProductionChains(
+      const std::vector<const industry::proto::Production *> &candidates,
+      std::vector<const industry::proto::Production *> *possible) const;
 };
 
 } // namespace geography
