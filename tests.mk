40ee390396231a011c4a05eca9848fcd03b0974c
kingofmen <rolf.andreassen@gmail.com>
2017-03-25 15:00:15 -0700

    Make test Makefile generic

--- tests.mk
+++ tests.mk
@@ -6,3 +6,24 @@ GTEST_SRCS_   = $(wildcard $(GTEST_DIR)/src/*.cc) $(wildcard $(GTEST_DIR)/src/*.
 INCPATH            += -I$(GTEST_DIR)/include
 CXXFLAGS         += -isystem $(GTEST_DIR)/include
 
+default: $(TARGET_NAME)_tests
+
+$(TARGET_NAME)_tests:	$(patsubst %test.cc,%test.o,$(wildcard *test.cc)) gtest_main.a
+			$(LINKER) $^ -o $@ $(LIBS)
+
+gtest-all.o:	$(GTEST_SRCS_)
+		$(CXX) $(CPPFLAGS) -I$(GTEST_DIR) $(CXXFLAGS) -c $(GTEST_DIR)/src/gtest-all.cc
+
+%test.o:	%test.cc
+		$(CXX) $(CXXFLAGS) $(INCPATH) -c -o $@ $<
+
+gtest_main.a:	gtest-all.o gtest_main.o
+		ar rcs $@ $^
+
+gtest_main.o:	$(GTEST_SRCS_)
+		$(CXX) $(CPPFLAGS) -I$(GTEST_DIR) $(CXXFLAGS) -c $(GTEST_DIR)/src/gtest_main.cc
+
+clean:
+		rm -f $(TARGET_NAME)_tests.exe
+		rm *.o
+
