1679c53a2a40c7a39d035e88398afc4dadb9ce2c
kingofmen <rolf.andreassen@gmail.com>
2017-04-05 00:28:50 -0700

    Parse game proto from human-readable file

--- game/sinews_main.cc
+++ game/sinews_main.cc
@@ -1,6 +1,32 @@
+#include <fstream>
 #include <iostream>
 
+#include "game/proto/game_world.pb.h"
+#include "geography/proto/geography.pb.h"
+#include "industry/proto/industry.pb.h"
+
+#include "google/protobuf/stubs/logging.h"
+#include "google/protobuf/text_format.h"
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+
 int main(int /*argc*/, char** /*argv*/) {
-  std::cout << "Hello, World\n";
+  std::ifstream game_reader("test_data\\test.pb.txt");
+  if (!game_reader.good()) {
+    std::cout << "Error opening file\n";
+    return 1;
+  }
+
+  game::proto::GameWorld game_world;
+  google::protobuf::io::IstreamInputStream input(&game_reader);
+  if (!google::protobuf::TextFormat::Parse(&input, &game_world)) {
+    std::cout << "Parsing error\n";
+    return 1;
+  }
+
+  game_reader.close();
+
+  std::cout << game_world.DebugString() << "\n";
+  
   return 0;
 }
+
