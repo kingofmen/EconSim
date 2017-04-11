adf9f4cc703c7ddcde05c5506d651d9b276b9909
kingofmen <rolf.andreassen@gmail.com>
2017-04-10 23:14:10 -0700

    Do autoproduction in executable

--- game/sinews_main.cc
+++ game/sinews_main.cc
@@ -1,13 +1,15 @@
 #include <fstream>
 #include <iostream>
+#include <memory>
 
 #include "game/proto/game_world.pb.h"
+#include "geography/geography.h"
 #include "geography/proto/geography.pb.h"
-#include "industry/proto/industry.pb.h"
-
 #include "google/protobuf/stubs/logging.h"
 #include "google/protobuf/text_format.h"
 #include <google/protobuf/io/zero_copy_stream_impl.h>
+#include "industry/proto/industry.pb.h"
+#include "population/popunit.h"
 
 int main(int /*argc*/, char** /*argv*/) {
   std::ifstream game_reader(".\\test_data\\test.pb.txt");
@@ -25,6 +27,26 @@ int main(int /*argc*/, char** /*argv*/) {
 
   game_reader.close();
 
+  std::vector<std::unique_ptr<population::PopUnit>> pops;
+  for (const auto& pop: game_world.pops()) {
+    pops.emplace_back(new population::PopUnit(pop));
+  }
+
+  std::vector<const population::proto::AutoProduction*> auto_production(
+      game_world.auto_production().pointer_begin(),
+      game_world.auto_production().pointer_end());
+
+  std::vector<std::unique_ptr<geography::Area>> areas;
+  for (const auto& area: game_world.areas()) {
+    areas.emplace_back(new geography::Area(area));
+  }
+  
+  for (auto& area: areas) {
+    for (const auto pop_id : area->pop_ids()) {
+      auto* pop = population::PopUnit::GetPopId(pop_id);
+      pop->AutoProduce(auto_production, area->GetPrices());
+    }
+  }
   std::cout << game_world.DebugString() << "\n";
 
   return 0;
