dde8835569a617313f7d45e956a97e40bd02a57d
kingofmen <rolf.andreassen@gmail.com>
2019-09-05 15:17:04 -0700

    FactionController from proto

--- factions/factions.cc
+++ factions/factions.cc
@@ -2,14 +2,32 @@
 
 namespace factions {
 
+std::unordered_map<uint64, FactionController*> FactionController::faction_map_;
+
 FactionController::FactionController(const proto::Faction& p) : proto_(p) {
   for (const uint64 pop_id : proto_.pop_ids()) {
     citizens_.insert(pop_id);
   }
+  faction_map_[id()] = this;
 }
 
 bool FactionController::IsFullCitizen(uint64 pop_id) const {
   return citizens_.find(pop_id) != citizens_.end();
 }
+
+FactionController* FactionController::GetByID(uint64 id) {
+  return faction_map_[id];
+}
+
+std::unique_ptr<FactionController> FactionController::FromProto(const proto::Faction& proto) {
+  std::unique_ptr<FactionController> ret;
+  // TODO: Actually handle the errors when we get support for StatusOr<unique_ptr>.
+  if (!proto.has_id()) {
+    return ret;
+  }
+  ret.reset(new FactionController(proto));
+  return ret;
+}
+
 
 }  // namespace factions
--- factions/factions.h
+++ factions/factions.h
@@ -1,6 +1,7 @@
 #ifndef FACTIONS_FACTIONS_H
 #define FACTIONS_FACTIONS_H
 
+#include <unordered_map>
 #include <unordered_set>
 
 #include "factions/proto/factions.pb.h"
@@ -11,23 +12,31 @@ namespace factions {
 // Information about the control and extent of a faction.
 class FactionController {
  public:
-  // Constructor.
-  FactionController(const proto::Faction& p);
+   static std::unique_ptr<FactionController>
+   FromProto(const proto::Faction& proto);
 
-  // Returns the faction id.
-  uint64 id() const { return proto_.id(); }
+   // Returns the faction id.
+   uint64 id() const { return proto_.id(); }
 
-  // Returns true if the POP is a direct member of the faction, a full citizen.
-  bool IsFullCitizen(uint64 pop_id) const;
+   // Returns true if the POP is a direct member of the faction, a full citizen.
+   bool IsFullCitizen(uint64 pop_id) const;
 
-  // Read access to the protobuf.
-  const proto::Faction& Proto() { return proto_; }
+   // Returns the controller with the given ID.
+   static FactionController* GetByID(uint64 id);
 
-private:
-  // Wire format.
-  proto::Faction proto_;
-  // Citizen IDs.
-  std::unordered_set<uint64> citizens_;
+   // Read access to the protobuf.
+   const proto::Faction& Proto() { return proto_; }
+
+ private:
+   // Constructor.
+   FactionController(const proto::Faction& p);
+
+   // Wire format.
+   proto::Faction proto_;
+   // Citizen IDs.
+   std::unordered_set<uint64> citizens_;
+   // Lookup map.
+   static std::unordered_map<uint64, FactionController*> faction_map_;
 };
 
 }  // namespace factions
