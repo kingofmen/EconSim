6c9073dd1ef6bc333d307196ab5d2c092716aeb3
kingofmen <rolf.andreassen@gmail.com>
2019-10-09 23:12:22 -0700

    One-and two-good optimisation calculations and tests.

--- population/consumption.cc
+++ population/consumption.cc
@@ -6,6 +6,7 @@
 #include "population/proto/consumption.pb.h"
 #include "util/arithmetic/microunits.h"
 #include "util/headers/int_types.h"
+#include "util/logging/logging.h"
 #include "util/status/status.h"
 
 namespace consumption {
@@ -16,26 +17,96 @@ util::Status internal_optimum_1(const market::proto::Container& goods,
                                 const market::proto::Container& prices,
                                 int64 offset_u, int64 dsquared_u,
                                 market::proto::Container* result) {
-  if (goods.quantities_size() != 1) {
+  if (goods.quantities().size() != 1) {
     return util::InvalidArgumentError(
         absl::Substitute("Optimum expected 1 good, got $0: $1",
-                         goods.quantities_size(), goods.DebugString()));
+                         goods.quantities().size(), goods.DebugString()));
   }
   result->Clear();
   *result += goods;
 
   return util::OkStatus();
 }
-util::Status internal_optimum_2(const market::proto::Container& goods,
-                                const market::proto::Container& prices,
-                                int64 offset_u, int64 dsquared_u,
-                                market::proto::Container* result) {
+
+util::Status calcX(int64 a, int64 b, int64 px, int64 py, int64 dsquared_u,
+            int64 offset_u, int64* value) {
+  uint64 overflow = 0;
+  int64 aC = micro::MultiplyU(py, dsquared_u);
+  aC = micro::MultiplyU(aC, a);
+  aC = micro::DivideU(aC, micro::MultiplyU(b, px), &overflow);
+  if (overflow != 0) {
+    return util::InvalidArgumentError(absl::Substitute(
+        "Division overflow with a=$0, b=$1, px=$2, py = $3, aC = $4", a, b, px,
+        py, aC));
+  }
+  VLOGF(5, "Coefficients %d, %d => aC = %d", a, b, aC);
+
+  int64 radical = micro::SqrtU(aC);
+  VLOGF(5, "Square root %d", radical);
+  *value = micro::DivideU(radical - offset_u, a);
+  if (overflow != 0) {
+    return util::InvalidArgumentError(absl::Substitute(
+        "Division overflow with radical=$0, offset = $1, a = $2", radical,
+        offset_u, a));
+  }
+  return util::OkStatus();
+}
+
+util::Status
+internal_optimum_2(const std::vector<market::proto::Quantity>& goods,
+                   const market::proto::Container& prices, int64 offset_u,
+                   int64 dsquared_u, market::proto::Container* result) {
+  if (goods.size() != 2) {
+    return util::InvalidArgumentError(
+        absl::Substitute("Optimum expected 2 goods, got $0", goods.size()));
+  }
+
+  market::proto::Container coeffs;
+  // TODO: Coefficients are constant, so can be calculated once and cached.
+  int64 a = -1;
+  int64 b = -1;
+  int64 px = -1;
+  int64 py = -1;
+  int64 oSquare = micro::MultiplyU(offset_u, offset_u);
+  for (const auto& good : goods) {
+    auto nom = dsquared_u - oSquare;
+    auto denom = micro::MultiplyU(good.amount(), offset_u);
+    auto ratio = micro::DivideU(nom, denom);
+    if (a < 0) {
+      a = ratio;
+      px = market::GetAmount(prices, good.kind());
+    } else {
+      b = ratio;
+      py = market::GetAmount(prices, good.kind());
+    }
+  }
+  VLOGF(5, "Calculating %s/%s with prices %d, %d", goods[0].kind(), goods[1].kind(), px, py);
+
+  int64 xValue = 0;
+  int64 yValue = 0;
+  auto status = calcX(a, b, px, py, dsquared_u, offset_u, &xValue);
+  if (!status.ok()) {
+    return status;
+  }
+  status = calcX(b, a, py, px, dsquared_u, offset_u, &yValue);
+  if (!status.ok()) {
+    return status;
+  }
+
+  market::SetAmount(goods[0].kind(), xValue, result);
+  market::SetAmount(goods[1].kind(), yValue, result);
+
   return util::OkStatus();
 }
 util::Status internal_optimum_3(const market::proto::Container& goods,
                                 const market::proto::Container& prices,
                                 int64 offset_u, int64 dsquared_u,
                                 market::proto::Container* result) {
+  if (goods.quantities().size() != 3) {
+    return util::InvalidArgumentError(
+        absl::Substitute("Optimum expected 3 goods, got $0: $1",
+                         goods.quantities().size(), goods.DebugString()));
+  }
   return util::OkStatus();
 }
 
@@ -43,18 +114,19 @@ namespace {
 util::Status internal_optimum(const market::proto::Container& goods,
                               const market::proto::Container& prices,
                               int64 offset_u, int64 dsquared_u,
-                              market::proto::Container* result) {
-  switch (goods.quantities_size()) {
+                              market::proto::Container* result) {  
+  switch (goods.quantities().size()) {
     case 1:
       return internal_optimum_1(goods, prices, offset_u, dsquared_u, result);
     case 2:
-      return internal_optimum_2(goods, prices, offset_u, dsquared_u, result);
+      return internal_optimum_2(market::Expand(goods), prices, offset_u,
+                                dsquared_u, result);
     case 3:
       return internal_optimum_3(goods, prices, offset_u, dsquared_u, result);
     default:
       return util::InvalidArgumentError(
           absl::Substitute("Optimum for $0 goods, can handle at most $1",
-                           goods.quantities_size(), kMaxSubstitutables));
+                           goods.quantities().size(), kMaxSubstitutables));
   }
   return util::OkStatus();
 }
@@ -64,13 +136,26 @@ util::Status internal_optimum(const market::proto::Container& goods,
 util::Status Optimum(const proto::Substitutes& subs,
                      const market::proto::Container& prices,
                      market::proto::Container* result) {
+  for (const auto& price : prices.quantities()) {
+    if (price.second < 1) {
+      return util::InvalidArgumentError(
+          absl::Substitute("$0: Prices must be positive, found $1 for $2",
+                           subs.name(), price.second, price.first));
+    }
+  }
   return internal_optimum(subs.consumed(), prices, subs.offset_u(),
                           subs.min_amount_square_u(), result);
 }
 
 util::Status Validate(const proto::Substitutes& subs) {
-  int numCon  = subs.consumed().quantities_size();
-  int numCap = subs.movable_capital().quantities_size();
+  if (subs.offset_u() <= 0) {
+    return util::InvalidArgumentError(
+        absl::Substitute("$0: Offset must be positive, found $1",
+                         subs.name(), subs.offset_u()));
+  }
+
+  int numCon  = subs.consumed().quantities().size();
+  int numCap = subs.movable_capital().quantities().size();
   if (0 == numCon + numCap) {
     return util::InvalidArgumentError(
         absl::Substitute("$0: Must specify at least one good", subs.name()));
@@ -86,25 +171,66 @@ util::Status Validate(const proto::Substitutes& subs) {
                          subs.name(), kMaxSubstitutables, numCap));
   }
 
+  int64 dsquared_u = subs.min_amount_square_u();
+  int64 offset_u = subs.offset_u();
   int oPowN = micro::kOneInU;
   for (int i = 0; i < numCon; ++i) {
-    oPowN = micro::MultiplyU(oPowN, subs.offset_u());
+    oPowN = micro::MultiplyU(oPowN, offset_u);
   }
-  if (subs.min_amount_square_u() <= oPowN) {
+  if (dsquared_u <= oPowN) {
     return util::InvalidArgumentError(absl::Substitute(
         "$0 consumables: D^2 must be strictly greater than o^n, found $1 <= $2",
-        subs.name(), subs.min_amount_square_u(), oPowN));
+        subs.name(), dsquared_u, oPowN));
   }
   oPowN = 1;
   for (int i = 0; i < numCap; ++i) {
-    oPowN = micro::MultiplyU(oPowN, subs.offset_u());
+    oPowN = micro::MultiplyU(oPowN, offset_u);
   }
-  if (subs.min_amount_square_u() <= oPowN) {
+  if (dsquared_u <= oPowN) {
     return util::InvalidArgumentError(absl::Substitute(
         "$0 capital: D^2 must be strictly greater than o^n, found $1 <= $2",
-        subs.name(), subs.min_amount_square_u(), oPowN));
+        subs.name(), dsquared_u, oPowN));
   }
 
+  for (const auto& good : subs.consumed().quantities()) {
+    auto nom = dsquared_u - micro::MultiplyU(offset_u, offset_u);
+    auto denom = micro::MultiplyU(good.second, offset_u);
+    uint64 overflow = 0;
+    auto ratio = micro::DivideU(nom, denom, &overflow);
+    if (overflow != 0) {
+      return util::InvalidArgumentError(absl::Substitute(
+          "$0 $1 (consumed): Coefficient ratio overflows, increase the "
+          "offset or crossing point",
+          subs.name(), good.first));
+    }
+    if (ratio < 1) {
+      return util::InvalidArgumentError(absl::Substitute(
+          "$0 $1 (consumed): Nonpositive coefficient ratio, D^2=$2, o=$3, "
+          "x_0=$4, "
+          "increase minimum or decrease offset",
+          subs.name(), good.first, dsquared_u, offset_u, good.second));
+    }
+  }
+
+  for (const auto& good : subs.movable_capital().quantities()) {
+    auto nom = dsquared_u - micro::MultiplyU(offset_u, offset_u);
+    auto denom = micro::MultiplyU(good.second, offset_u);
+    uint64 overflow = 0;
+    auto ratio = micro::DivideU(nom, denom, &overflow);
+    if (overflow != 0) {
+      return util::InvalidArgumentError(absl::Substitute(
+          "$0 $1 (capital): Coefficient ratio overflows, increase the "
+          "offset or crossing point",
+          subs.name(), good.first));
+    }
+    if (ratio < 1) {
+      return util::InvalidArgumentError(
+          absl::Substitute("$0 $1 (capital): Nonpositive coefficient ratio, "
+                           "increase minimum or decrease offset",
+                           subs.name(), good.first));
+    }
+  }
+  
   return util::OkStatus();
 }
 
--- population/consumption_test.cc
+++ population/consumption_test.cc
@@ -6,6 +6,7 @@
 #include "market/goods_utils.h"
 #include "population/proto/consumption.pb.h"
 #include "util/arithmetic/microunits.h"
+#include "util/logging/logging.h"
 
 namespace consumption {
 namespace {
@@ -19,17 +20,52 @@ TEST(ConsumptionTest, Optimum) {
   proto::Substitutes subs;
   market::proto::Container result;
   market::proto::Container prices;
+  Log::SetVerbosity("population/consumption.cc", 10);
+  Log::Register(Log::coutLogger);
 
   market::Add(kApples, 3*micro::kOneInU, subs.mutable_consumed());
+  EXPECT_OK(Validate(subs));
   EXPECT_OK(Optimum(subs, prices, &result));
   EXPECT_EQ(3*micro::kOneInU, market::GetAmount(result, kApples));
   EXPECT_EQ(0, market::GetAmount(result, kOranges));
 
   // Initially test symmetric case.
+  result.Clear();
   market::Add(kOranges, 3*micro::kOneInU, subs.mutable_consumed());
   market::SetAmount(kApples, micro::kOneInU, &prices);
   market::SetAmount(kOranges, micro::kOneInU, &prices);
+  EXPECT_OK(Validate(subs));
   EXPECT_OK(Optimum(subs, prices, &result));
+  EXPECT_EQ(micro::kOneInU, market::GetAmount(result, kApples));
+  EXPECT_EQ(micro::kOneInU, market::GetAmount(result, kOranges));
+
+  // More extreme symmetric case.
+  market::Add(kApples, 7*micro::kOneInU, subs.mutable_consumed());
+  market::Add(kOranges, 7*micro::kOneInU, subs.mutable_consumed());
+  EXPECT_OK(Validate(subs));
+  EXPECT_OK(Optimum(subs, prices, &result));
+  EXPECT_EQ(3333333, market::GetAmount(result, kApples));
+  EXPECT_EQ(3333333, market::GetAmount(result, kOranges));
+
+  // Asymmetric price.
+  market::SetAmount(kApples, 2*micro::kOneInU, &prices);
+  market::SetAmount(kApples, 3*micro::kOneInU, subs.mutable_consumed());
+  market::SetAmount(kOranges, 3*micro::kOneInU, subs.mutable_consumed());
+  EXPECT_OK(Optimum(subs, prices, &result));
+  // Respectively sqrt{2} - 1 and 2\sqrt{2} - 1.
+  EXPECT_EQ(414214, market::GetAmount(result, kApples));
+  EXPECT_EQ(1828428, market::GetAmount(result, kOranges));
+
+  // Asymmetric crossing points.
+  market::SetAmount(kApples, micro::kOneInU, &prices);
+  market::SetAmount(kApples, 3*micro::kOneInU, subs.mutable_consumed());
+  market::SetAmount(kOranges, 1*micro::kOneInU, subs.mutable_consumed());
+  EXPECT_OK(Validate(subs));
+  EXPECT_OK(Optimum(subs, prices, &result));
+  // -1+2sqrt{1/3}.
+  EXPECT_EQ(154700, market::GetAmount(result, kApples));
+  // (-1 + 2\sqrt{3})/3.
+  EXPECT_EQ(821367, market::GetAmount(result, kOranges));
 }
 
 TEST(ConsumptionTest, Validation) {
@@ -56,6 +92,10 @@ TEST(ConsumptionTest, Validation) {
               testing::HasSubstr("consumables: D^2 must be strictly greater"));
 
   subs.set_offset_u(2*micro::kOneInU-1);
+  status = Validate(subs);
+  EXPECT_THAT(status.error_message(),
+              testing::HasSubstr("consumed): Nonpositive coefficient ratio"));
+  subs.set_offset_u(3*micro::kOneInU/2);
   status = Validate(subs);
   EXPECT_OK(status) << status.error_message();
 
