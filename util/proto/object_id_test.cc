d8600d22a2437b32fca5b898dfe347c86ddad63d
kingofmen <rolf.andreassen@gmail.com>
2020-03-29 16:12:58 -0700

    Tests for ObjectID tags.

---
+++ util/proto/object_id_test.cc
@@ -0,0 +1,58 @@
+#include "util//proto/object_id.h"
+
+#include "absl/strings/str_join.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "util/proto/object_id.pb.h"
+#include "util/status/status.h"
+
+namespace util {
+namespace objectid {
+
+TEST(ObjectId, TestTags) {
+  util::proto::ObjectId obj_id;
+  auto status = Canonicalise(&obj_id);
+  EXPECT_THAT(status.error_message(), testing::HasSubstr("without type"));
+
+  obj_id.set_type(1);
+  status = Canonicalise(&obj_id);
+  EXPECT_THAT(status.error_message(), testing::HasSubstr("with only type"));
+
+  obj_id.set_number(1);
+  status = Canonicalise(&obj_id);
+  EXPECT_OK(status) << status.error_message();
+
+  obj_id.set_tag("one");
+  status = Canonicalise(&obj_id);
+  EXPECT_OK(status) << status.error_message();
+  EXPECT_FALSE(obj_id.has_tag());
+  EXPECT_EQ(1, obj_id.number());
+
+  obj_id.set_tag("one");
+  status = Canonicalise(&obj_id);
+  EXPECT_THAT(status.error_message(),
+              testing::HasSubstr("(number) already exists"));
+
+  obj_id.set_number(2);
+  status = Canonicalise(&obj_id);
+  EXPECT_THAT(status.error_message(),
+              testing::HasSubstr("(tag) already exists"));
+
+  obj_id.clear_number();
+  status = Canonicalise(&obj_id);
+  EXPECT_OK(status) << status.error_message();
+  EXPECT_FALSE(obj_id.has_tag());
+  EXPECT_EQ(1, obj_id.number());
+
+  obj_id.set_tag("two");
+  obj_id.clear_number();
+  status = Canonicalise(&obj_id);
+  EXPECT_THAT(status.error_message(), testing::HasSubstr("Cannot find number"));
+
+  obj_id.clear_tag();
+  obj_id.set_number(1);
+  EXPECT_EQ(Tag(obj_id), "one");
+}
+
+}  // namespace objectid
+}  // namespace util
