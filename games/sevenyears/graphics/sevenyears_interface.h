35d0494da5899fa2f8cfffef3d78a799ed6e47c9
kingofmen <rolf.andreassen@gmail.com>
2020-03-31 22:07:12 -0700

    Update SevenYearsInterface for new location of interface

--- games/sevenyears/graphics/sevenyears_interface.h
+++ games/sevenyears/graphics/sevenyears_interface.h
@@ -2,8 +2,8 @@
 #define GAMES_SEVENYEARS_GRAPHICS_SEVENYEARS_INTERFACE_H
 
 #include "games/sevenyears/graphics/proto/graphics.pb.h"
-#include "interface/base.h"
-#include "interface/proto/config.pb.h"
+#include "games/interface/base.h"
+#include "games/interface/proto/config.pb.h"
 #include "util/status/status.h"
 
 namespace sevenyears {
@@ -11,7 +11,8 @@ namespace graphics {
 
 class SevenYearsInterface : public interface::Base {
 public:
-  virtual util::Status Initialise(const interface::proto::Config& config) = 0;
+  virtual util::Status
+  Initialise(const games::interface::proto::Config& config) = 0;
   virtual void Cleanup() = 0;
   virtual void EventLoop() = 0;
 
