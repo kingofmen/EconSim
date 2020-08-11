afbe6536dcb538d1c93eaae335d8d529aa6eec65
kingofmen <rolf.andreassen@gmail.com>
2020-08-11 23:13:47 +0200

    Added NumTurns test with lower speed.

--- games/ai/impl/utils_test.cc
+++ games/ai/impl/utils_test.cc
@@ -16,6 +16,29 @@ TEST_F(AiUtilsTest, TestNumTurns) {
   std::vector<uint64> path;
   path.push_back(connection_12->connection_id());
   EXPECT_EQ(1, NumTurns(*unit_, path));
+  path.push_back(connection_23->connection_id());
+  EXPECT_EQ(2, NumTurns(*unit_, path));
+}
+
+TEST_F(AiUtilsTest, TestLowerSpeed) {
+  units::proto::Template temp;
+  temp.mutable_template_id()->set_kind("slow");
+  temp.mutable_mobility()->set_speed_u(micro::kTwoThirdsInU);
+  temp.set_base_action_points_u(micro::kOneInU);
+  units::Unit::RegisterTemplate(temp);
+
+  units::proto::Unit unit;
+  unit.mutable_unit_id()->set_kind("slow");
+  unit.mutable_unit_id()->set_number(2);
+  *unit.mutable_location()->mutable_a_area_id() = area1_->area_id();
+  unit_ = units::Unit::FromProto(unit);
+
+  std::vector<uint64> path;
+  path.push_back(connection_12->connection_id());
+  EXPECT_EQ(2, NumTurns(*unit_, path));
+  path.push_back(connection_23->connection_id());
+  // TODO(Issue 33, issue 34): This should be 3 when these issues are fixed.
+  EXPECT_EQ(4, NumTurns(*unit_, path));
 }
 
 } // namespace impl
