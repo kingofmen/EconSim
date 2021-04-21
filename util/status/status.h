9db3cb0cedd7c4a9cd83ec49909b5485029a8110
kingofmen <rolf.andreassen@gmail.com>
2021-04-21 15:12:14 +0200

    Add FooErrorf methods to auto-format Status error messages.

--- util/status/status.h
+++ util/status/status.h
@@ -5,6 +5,7 @@
 
 #include <string>
 
+#include "absl/strings/str_format.h"
 #include "src/google/protobuf/stubs/status.h"
 
 namespace util {
@@ -18,6 +19,38 @@ Status FailedPreconditionError(const std::string& msg);
 Status NotFoundError(const std::string& msg);
 Status NotImplementedError(const std::string& msg);
 Status ResourceExhaustedError(const std::string& msg);
+
+template <typename... Args>
+Status AlreadyExistsErrorf(const absl::FormatSpec<Args...>& format,
+                           const Args&... args) {
+  return AlreadyExistsError(absl::StrFormat(format, args...));
+}
+
+template <typename... Args>
+Status InvalidArgumentErrorf(const absl::FormatSpec<Args...>& format,
+                             const Args&... args) {
+  return InvalidArgumentError(absl::StrFormat(format, args...));
+}
+template <typename... Args>
+Status FailedPreconditionErrorf(const absl::FormatSpec<Args...>& format,
+                                const Args&... args) {
+  return FailedPreconditionError(absl::StrFormat(format, args...));
+}
+template <typename... Args>
+Status NotFoundErrorf(const absl::FormatSpec<Args...>& format,
+                      const Args&... args) {
+  return NotFoundError(absl::StrFormat(format, args...));
+}
+template <typename... Args>
+Status NotImplementedErrorf(const absl::FormatSpec<Args...>& format,
+                            const Args&... args) {
+  return NotImplementedError(absl::StrFormat(format, args...));
+}
+template <typename... Args>
+Status ResourceExhaustedErrorf(const absl::FormatSpec<Args...>& format,
+                               const Args&... args) {
+  return ResourceExhaustedError(absl::StrFormat(format, args...));
+}
 
 // Statuses are equal if they have the same code and message.
 bool Equal(const Status& one, const Status& two);
