2116b08787e5dcffc669fd728017ef2575ba183a
kingofmen <rolf.andreassen@gmail.com>
2021-04-25 13:36:50 +0200

    Issue #18: Remove random industry proto from file test, use object ID from the file package instead.

--- util/proto/file_test.cc
+++ util/proto/file_test.cc
@@ -4,42 +4,47 @@
 
 #include "absl/strings/str_join.h"
 #include "gtest/gtest.h"
-#include "games/industry/proto/industry.pb.h"
 #include "util/status/status.h"
+#include "util/proto/object_id.pb.h"
 
 namespace util {
 namespace proto {
 
+const std::string kTestDir = std::getenv("TEST_SRCDIR");
+const std::string kWorkdir = std::getenv("TEST_WORKSPACE");
 const std::string kTestDataLocation = "util/proto/test_data";
-  // This is a workaround for Bazel issues 4102 and 4292. When they are
-  // fixed, use TEST_SRCDIR/TEST_WORKSPACE instead.
-  const std::string kTestDir = "C:/Users/Rolf/base";
 
 TEST(ProtoUtils, TestParseProtoFile) {
-  industry::proto::Production proto;
-  const std::string filename = "industry.pb.txt";
+  ObjectId proto;
+  const std::string filename = "objectid.pb.txt";
   auto status = ParseProtoFile(
-      absl::StrJoin({kTestDir, kTestDataLocation, filename}, "/"), &proto);
+      absl::StrJoin({kTestDir, kWorkdir, kTestDataLocation, filename}, "/"),
+      &proto);
   EXPECT_OK(status) << status.error_message();
-  EXPECT_EQ(industry::proto::LT_FIELDS, proto.land_type())
-      << proto.DebugString();
+  EXPECT_EQ("test", proto.kind()) << proto.DebugString();
+  EXPECT_EQ(1, proto.number()) << proto.DebugString();
+  EXPECT_EQ("check", proto.tag()) << proto.DebugString();
 }
 
 TEST(ProtoUtils, TestMergeProtoFile) {
-  industry::proto::Production proto;
-  const std::string filename = "industry.pb.txt";
+  ObjectId proto;
+  const std::string filename = "objectid.pb.txt";
   auto status = MergeProtoFile(
-      absl::StrJoin({kTestDir, kTestDataLocation, filename}, "/"), &proto);
+      absl::StrJoin({kTestDir, kWorkdir, kTestDataLocation, filename}, "/"),
+      &proto);
   EXPECT_OK(status) << status.error_message();
-  EXPECT_EQ(industry::proto::LT_FIELDS, proto.land_type())
-      << proto.DebugString();
-  EXPECT_EQ(1, proto.steps_size()) << proto.DebugString();
+  EXPECT_EQ("test", proto.kind()) << proto.DebugString();
+  EXPECT_EQ(1, proto.number()) << proto.DebugString();
+  EXPECT_EQ("check", proto.tag()) << proto.DebugString();
+  
+  proto.clear_tag();
   status = MergeProtoFile(
-      absl::StrJoin({kTestDir, kTestDataLocation, filename}, "/"), &proto);
+      absl::StrJoin({kTestDir, kWorkdir, kTestDataLocation, filename}, "/"),
+      &proto);
   EXPECT_OK(status) << status.error_message();
-  EXPECT_EQ(industry::proto::LT_FIELDS, proto.land_type())
-      << proto.DebugString();
-  EXPECT_EQ(2, proto.steps_size()) << proto.DebugString();
+  EXPECT_EQ("test", proto.kind()) << proto.DebugString();
+  EXPECT_EQ(1, proto.number()) << proto.DebugString();
+  EXPECT_EQ("check", proto.tag()) << proto.DebugString();
 }
 
 }  // namespace proto
