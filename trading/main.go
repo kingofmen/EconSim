493b0d1663bfde288c179275d79c312ce4fb1d36
kingofmen <rolf.andreassen@gmail.com>
2022-07-14 18:27:28 -0700

    Default today's date, allow override with argument.

--- trading/main.go
+++ trading/main.go
@@ -8,17 +8,47 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 const (
 	apiToken = "lEKU4KMzBOJWda2UaVN7TeOsskQSMBs2"
-	apiURL = "https://api.polygon.io/v2/aggs/ticker/O:%s210903C00700000/range/1/day/2021-07-22/2021-07-22"
+	apiURL = "https://api.polygon.io/v2/aggs/ticker/O:%s210903C00700000/range/1/day/%s/%s"
 )
 
 // PolyAggregate is a model of the JSON returned by Polygon's 'aggs' endpoint.
 type PolyAggregate struct {
 	Ticker string `json:"ticker"`
 	Results []map[string]float64 `json:"results"`
+}
+
+type urlBuilder struct {
+	ticker string
+	date string
+}
+
+func todayString() string {
+	year, month, day := time.Now().Date()
+	return fmt.Sprintf("%d-%02d-%d", year, int(month), day)
+}
+
+func newURLBuilder(args []string) *urlBuilder {
+	ub := &urlBuilder{
+		ticker: "TSLA",
+		date: todayString(),
+	}
+
+	if len(args) > 0 {
+		ub.ticker = args[0]
+	}
+	if len(args) > 1 {
+		ub.date = args[1]
+	}
+	return ub
+}
+
+func (ub *urlBuilder) makeURL() string {
+	return fmt.Sprintf(apiURL, ub.ticker, ub.date, ub.date)
 }
 
 func handlePolyResponse(res *http.Response) error {
@@ -36,12 +66,8 @@ func handlePolyResponse(res *http.Response) error {
 	return nil
 }
 
-func makeURL(ticker string) string {
-	return fmt.Sprintf(apiURL, ticker)
-}
-
-func makeRequest(ticker string) (*http.Request, error) {
-	req, err := http.NewRequest(http.MethodGet, makeURL(ticker), nil)
+func makeRequest(url string) (*http.Request, error) {
+	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -50,12 +76,8 @@ func makeRequest(ticker string) (*http.Request, error) {
 }
 
 func main() {
-	ticker := "TSLA"
-	if len(os.Args) > 1 {
-		ticker = os.Args[1]
-	}
-
-	req, err := makeRequest(ticker)
+	urlBuilder := newURLBuilder(os.Args[1:])
+	req, err := makeRequest(urlBuilder.makeURL())
 	if err != nil {
 		log.Fatalf("Couldn't create request: %v", err)
 	}
