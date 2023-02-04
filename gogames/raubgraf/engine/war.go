659e20e47754671ad675c2a7f92456369a9b1aa7
kingofmen <rolf.andreassen@gmail.com>
2023-02-04 15:22:35 -0800

    Stub war package.

---
+++ gogames/raubgraf/engine/war.go
@@ -0,0 +1,11 @@
+package war
+
+import (
+  "gogames/raubgraf/engine/pop"
+)
+
+// Battle returns true if the attackers win.
+func Battle(attackers, defenders []*pop.Pop) bool {
+  // TODO: Actual calculation here.
+  return true
+}
