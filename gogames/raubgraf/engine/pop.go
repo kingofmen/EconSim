404ac9c4e1b531bdce6b5da16f32a7607f5c4192
kingofmen <rolf.andreassen@gmail.com>
2023-02-04 11:31:07 -0800

    Add busy-tracking to Pops.

--- gogames/raubgraf/engine/pop.go
+++ gogames/raubgraf/engine/pop.go
@@ -14,14 +14,14 @@ const (
 
 type Filter func(p *Pop) bool
 
+// AvailableFilter passes pops that have not acted this turn.
+func AvailableFilter(p *Pop) bool {
+  return !p.Busy()
+}
+
 // BanditFilter passes pops that are bandits.
 func BanditFilter(p *Pop) bool {
   return p.GetKind() == Bandit
-}
-
-// PeasantFilter passes pops that are peasants.
-func PeasantFilter(p *Pop) bool {
-  return p.GetKind() == Peasant
 }
 
 // HungryFilter passes pops that are hungry.
@@ -34,10 +34,33 @@ func NotHungryFilter(p *Pop) bool {
   return p.GetHunger() == 0
 }
 
+// PeasantFilter passes pops that are peasants.
+func PeasantFilter(p *Pop) bool {
+  return p.GetKind() == Peasant
+}
+
 // Pop models a group of several dozen humans.
 type Pop struct {
   kind Kind
   hungry int
+  busy bool
+}
+
+// Busy returns true if the Pop has acted.
+func (p *Pop) Busy() bool {
+  if p == nil {
+    // The nil Pop cannot do anything, so is always busy.
+    return true
+  }
+  return p.busy
+}
+
+// Clear readies the Pop for a new turn.
+func (p *Pop) Clear() {
+  if p == nil {
+    return
+  }
+  p.busy = false
 }
 
 // Copy returns a copy of the Pop.
@@ -93,6 +116,16 @@ func (p *Pop) GetHunger() int {
   return p.hungry
 }
 
+// Work sets the pop's busy state, returning false if it was already busy.
+func (p *Pop) Work() bool {
+  if p == nil {
+    return false
+  }
+  b := p.busy
+  p.busy = true
+  return !b
+}
+
 // String returns a human-readable string suitable for debugging.
 // It is not intended for display to users.
 func (k Kind) String() string {
@@ -120,5 +153,6 @@ func New(k Kind) *Pop {
   return &Pop{
     kind: k,
     hungry: 0,
+    busy: false,
   }
 }
