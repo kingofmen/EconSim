5ab60dcac3cd7bea070115f3704b39d71e95f5ff
kingofmen <rolf.andreassen@gmail.com>
2023-02-18 13:55:48 -0800

    Add debug string to Point.

--- gogames/util/coords.go
+++ gogames/util/coords.go
@@ -1,11 +1,23 @@
 package coords
 
+import (
+  "fmt"
+)
+
 type Point [2]int
 
+// X returns the point's x (first) coordinate.
 func (p Point) X() int {
   return p[0]
 }
 
+// Y returns the point's y (second) coordinate.
 func (p Point) Y() int {
   return p[1]
+}
+
+// String returns a human-readable debugging string;
+// it is not intended to be user-visible.
+func (p Point) String() string {
+  return fmt.Sprintf("(%d, %d)", p.X(), p.Y())
 }
