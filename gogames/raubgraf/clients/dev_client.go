44c04908775c01383cad71d19d0f9b5f36ef3398
kingofmen <rolf.andreassen@gmail.com>
2023-03-05 22:19:08 -0800

    Stub dev_client package.

---
+++ gogames/raubgraf/clients/dev_client.go
@@ -0,0 +1,20 @@
+// Package devclient is a CLI interface to the Raubgraf engine.
+// It is intended for development and debugging.
+package devclient
+
+import(
+
+)
+
+// Display writes the game state to the shell.
+func Display() error {
+  // TODO: Implement.
+  return nil
+}
+
+// Parse parses a user command.
+func Parse() error {
+  // TODO: Implement.
+  return nil
+}
+
