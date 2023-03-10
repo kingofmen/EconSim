56a63aafd9320f64945758a1728663a536355299
kingofmen <rolf.andreassen@gmail.com>
2023-03-09 23:41:43 -0800

    Stub game handler.

--- gogames/raubgraf/clients/dev/client_lib.go
+++ gogames/raubgraf/clients/dev/client_lib.go
@@ -5,7 +5,10 @@ package clientlib
 import (
 	"errors"
 	"fmt"
+	"strconv"
 	"strings"
+
+	"gogames/raubgraf/engine/engine"
 )
 
 type InputFormat int
@@ -32,6 +35,10 @@ type Handler interface {
 type startHandler struct {
 }
 
+type gameHandler struct {
+	game *engine.RaubgrafGame
+}
+
 func clear() {
 	for idx := range lines {
 		lines[idx] = strings.Repeat(" ", width)
@@ -47,6 +54,21 @@ func Flip() {
 
 func (h *startHandler) Format() InputFormat {
 	return IFChar
+}
+
+func (h *gameHandler) Format() InputFormat {
+	return IFChar
+}
+
+func (h *gameHandler) Display() {
+	clear()
+}
+
+func (h *gameHandler) Parse(inp string) (Handler, error) {
+	if strings.ToLower(inp) == "q" {
+		return h, QuitSignal
+	}
+	return h, nil
 }
 
 func (h *startHandler) Display() {
@@ -66,9 +88,17 @@ func (h *startHandler) Display() {
 	lines[22] = "                 RRR    RRR       AAA               AAA      UUU         UUU     BBB    BBB    GGG        GGG  RRR    RRR      AAA               AAA    FFF         "
 	lines[23] = "                 RRR     RRR      AAA               AAA       UUUUUUUUUUUUU      BBBBBBBBB      GGGGGGGGGGGG   RRR     RRR     AAA               AAA    FFF         "
 	lines[24] = "                 RRR      RRR    AAAA               AAAA        UUUUUUUUU       BBBBBBBBB        GGGGGGGGGG    RRR      RRR   AAAA               AAAA   FFF         "
+
+	lines[30] = strings.Repeat(" ", 100) + "(Q)uit"
+	lines[31] = strings.Repeat(" ", 100) + "New game (5)x5"
+	lines[32] = strings.Repeat(" ", 100) + "New game (1)0x10"
+	lines[33] = strings.Repeat(" ", 100) + "New game (2)0x20"
+	lines[34] = strings.Repeat(" ", 100) + "(L)oad game (not implemented)"
+
 	for idx := range lines {
 		ll := len(lines[idx])
 		if ll >= width {
+			lines[idx] = lines[idx][:width]
 			continue
 		}
 		lines[idx] += strings.Repeat(" ", width-ll)
@@ -79,6 +109,24 @@ func (h *startHandler) Parse(inp string) (Handler, error) {
 	if strings.ToLower(inp) == "q" {
 		return h, QuitSignal
 	}
+	if inp == "1" || inp == "2" || inp == "5" {
+		size, err := strconv.Atoi(inp)
+		if err != nil {
+			return h, err
+		}
+		if size < 5 {
+			size *= 10
+		}
+		game, err := engine.NewGame(size, size)
+		if err != nil {
+			return h, err
+		}
+		return &gameHandler{game: game}, nil
+	}
+	if strings.ToLower(inp) == "l" {
+		return h, fmt.Errorf("Game loading not implemented.")
+	}
+
 	return h, nil
 }
 
