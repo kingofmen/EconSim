5b8adebee40fbd0a3bd6b258c9afa2ad45da76be
kingofmen <rolf.andreassen@gmail.com>
2024-01-08 23:40:18 -0800

    Stub tick loop.

--- gogames/settlers/engine/settlers.go
+++ gogames/settlers/engine/settlers.go
@@ -1,6 +1,19 @@
 // Package settlers defines the core gameplay loop.
 package settlers
 
+import (
+	"fmt"
+)
+
+type Activity int
+
+const (
+	CONSUME = iota // Eat, drink, and be merry; all that feeds the body.
+	MEANING        // Ritual, sport, family, worship; all that makes life worth living.
+	PRODUCE        // Investment, building, the creation of tools.
+	MILITIA        // Drill, weapons, training, patrol; the defense of the other three.
+)
+
 // Faction models a tribe or nation.
 type Faction struct {
 	// Number of people not assigned to a specific settlement.
@@ -9,6 +22,19 @@ type Faction struct {
 
 // Settlement contains the production logic.
 type Settlement struct {
+	priorities [4]int16
+}
+
+// Prioritize calculates the production fractions.
+func (s *Settlement) Prioritize() {
+	if s == nil {
+		return
+	}
+	// TODO: Replace this placeholder stub with an actual calculation!
+	s.priorities[CONSUME] = 250
+	s.priorities[MEANING] = 250
+	s.priorities[PRODUCE] = 250
+	s.priorities[MILITIA] = 250
 }
 
 // Map contains the game board.
@@ -23,5 +49,13 @@ func (m *Map) Place() error {
 
 // Tick runs all economic and military logic for one timestep.
 func (m *Map) Tick() error {
+	if m == nil {
+		return fmt.Errorf("Tick called on nil Map")
+	}
+
+	for _, s := range m.settlements {
+		s.Prioritize()
+	}
+
 	return nil
 }
