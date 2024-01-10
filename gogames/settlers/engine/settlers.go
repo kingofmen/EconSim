839e0d5d018e6f8149c2d542b7ded883b1522ebe
kingofmen <rolf.andreassen@gmail.com>
2024-01-09 21:58:45 -0800

    Rename Settlement to Piece, Map to Board; implement Place method using Rule interface.

--- gogames/settlers/engine/settlers.go
+++ gogames/settlers/engine/settlers.go
@@ -3,6 +3,8 @@ package settlers
 
 import (
 	"fmt"
+
+	"gogames/util/coords"
 )
 
 type Activity int
@@ -20,20 +22,37 @@ type Faction struct {
 	Freemen int64
 }
 
-// Template contains constant settlement data.
-type Template struct {
-	people int64
+// Rule models a requirement for a piece to be placed.
+type Rule interface {
+	Allow(pos coords.Point, fac *Faction, board *Board) error
 }
 
-// Settlement contains variable production data.
-type Settlement struct {
+// Template contains information shared across a class of game pieces,
+// such as shape and production capability.
+type Template struct {
+	people int64
+
+	required []Rule
+}
+
+// Piece is an instantiation of a Template, placed on the board.
+type Piece struct {
 	kind       *Template
 	faction    *Faction
 	priorities [4]int16
 	people     int64
 }
 
-func (s *Settlement) Populate() {
+func NewPiece(tmp *Template, fac *Faction) *Piece {
+	return &Piece{
+		kind:       tmp,
+		faction:    fac,
+		priorities: [4]int16{250, 250, 250, 250},
+		people:     0,
+	}
+}
+
+func (s *Piece) Populate() {
 	if s == nil {
 		return
 	}
@@ -50,7 +69,7 @@ func (s *Settlement) Populate() {
 }
 
 // Prioritize calculates the production fractions.
-func (s *Settlement) Prioritize() {
+func (s *Piece) Prioritize() {
 	if s == nil {
 		return
 	}
@@ -61,25 +80,43 @@ func (s *Settlement) Prioritize() {
 	s.priorities[MILITIA] = 250
 }
 
-// Map contains the game board.
-type Map struct {
-	settlements []*Settlement
+func (s *Piece) Produce() {
+	if s == nil {
+		return
+	}
 }
 
-// Place puts a Settlement onto the map.
-func (m *Map) Place() error {
+// Board contains the game board.
+type Board struct {
+	settlements []*Piece
+}
+
+// Place puts a Piece onto the board.
+func (m *Board) Place(pos coords.Point, fac *Faction, tmp *Template) []error {
+	bad := make([]error, 0, len(tmp.required))
+	for _, rule := range tmp.required {
+		if err := rule.Allow(pos, fac, m); err != nil {
+			bad = append(bad, err)
+		}
+	}
+	if len(bad) > 0 {
+		return bad
+	}
+
+	m.settlements = append(m.settlements, NewPiece(tmp, fac))
 	return nil
 }
 
 // Tick runs all economic and military logic for one timestep.
-func (m *Map) Tick() error {
+func (m *Board) Tick() error {
 	if m == nil {
-		return fmt.Errorf("Tick called on nil Map")
+		return fmt.Errorf("Tick called on nil Board")
 	}
 
 	for _, s := range m.settlements {
 		s.Prioritize()
 		s.Populate()
+		s.Produce()
 	}
 
 	return nil
