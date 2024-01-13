3ec6ba93068652211ae7c2e985780a0a923b8a87
kingofmen <rolf.andreassen@gmail.com>
2024-01-13 00:16:28 -0800

    Ebiten client now displays a trivial moving square.

--- gogames/settlers/clients/ebiten/main.go
+++ gogames/settlers/clients/ebiten/main.go
@@ -1,20 +1,34 @@
 package main
 
 import (
+	"image/color"
 	"log"
 
+	"gogames/settlers/engine/settlers"
+
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	_ "github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+type Game struct {
+	board   *settlers.Board
+	testImg *ebiten.Image
+	opts    *ebiten.DrawImageOptions
+	count   float64
+}
 
 func (g *Game) Update() error {
+	g.count++
+	if g.count > 100 {
+		g.count = 0
+		g.opts.GeoM.Reset()
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	g.opts.GeoM.Translate(g.count, g.count)
+	screen.DrawImage(g.testImg, g.opts)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -24,7 +38,20 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func main() {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	board, err := settlers.NewHex(3)
+	if err != nil {
+		log.Fatal(err)
+	}
+	game := &Game{
+		board:   board,
+		testImg: ebiten.NewImage(10, 10),
+		opts: &ebiten.DrawImageOptions{
+			GeoM: ebiten.GeoM{},
+		},
+		count: 0,
+	}
+	game.testImg.Fill(color.White)
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
