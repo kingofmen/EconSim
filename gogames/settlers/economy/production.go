d1171353f7c09052638c3a58c7bcd188446cfba0
kingofmen <rolf.andreassen@gmail.com>
2024-01-31 23:44:55 -0800

    Production logic; test for goods filter.

---
+++ gogames/settlers/economy/production.go
@@ -0,0 +1,108 @@
+// Package production models productive economic activity.
+package production
+
+import (
+	prodpb "gogames/settlers/economy/production_proto"
+)
+
+const (
+	tolerance = 0.001
+)
+
+type Producer interface {
+	Makes() string
+	Output() float64
+	Run(map[string]float64) float64
+}
+
+// meets returns true if exists matches the requirement.
+func meets(exist map[string]float64, buckets map[string]*prodpb.Good, req *prodpb.Requirement) bool {
+	switch req.GetKind() {
+	case prodpb.Requirement_RK_FULL:
+		target, ok := buckets[req.GetKey()]
+		if !ok {
+			return false
+		}
+		return exist[req.GetKey()] >= target.GetMaximum()
+	case prodpb.Requirement_RK_CAP:
+		return exist[req.GetKey()] <= req.GetAmount()
+	case prodpb.Requirement_RK_MIN:
+		return exist[req.GetKey()] >= req.GetAmount()
+	}
+	return false
+}
+
+// filterGoods returns the bucket keys whose requirements are met by exist.
+func filterGoods(exist map[string]float64, buckets map[string]*prodpb.Good) map[string]bool {
+	possible := make(map[string]bool)
+bucketLoop:
+	for key, good := range buckets {
+		for _, req := range good.GetPrereqs() {
+			if !meets(exist, buckets, req) {
+				continue bucketLoop
+			}
+		}
+		possible[key] = true
+	}
+	return possible
+}
+
+func calcAlloc(exist map[string]float64, buckets map[string]*prodpb.Good) map[prodpb.Area]float64 {
+	alloc := map[prodpb.Area]float64{
+		prodpb.Area_A_CONSUME:  0.01,
+		prodpb.Area_A_MEANING:  0.01,
+		prodpb.Area_A_INVEST:   0.01,
+		prodpb.Area_A_MILITARY: 0.01,
+	}
+	total := 0.04
+	for key, good := range buckets {
+		amt := exist[key]
+		if amt < tolerance {
+			continue
+		}
+		alloc[good.GetArea()] += amt
+		total += amt
+	}
+	invTotal := 1.0 / total
+	for key, val := range alloc {
+		alloc[key] = val * invTotal
+	}
+	return alloc
+}
+
+// Produce walks the decision tree for the given producers and
+// runs the resulting production functions.
+// TODO: Return a slice of decisions made.
+func Produce(labor float64, exist map[string]float64, buckets map[string]*prodpb.Good, proc []Producer, alloc map[prodpb.Area]float64) {
+	count := 0
+	for labor > tolerance {
+		count++
+		if count > 1000 {
+			return
+		}
+		available := filterGoods(exist, buckets)
+		if len(available) == 0 {
+			return
+		}
+
+		currAlloc := calcAlloc(exist, buckets)
+		best := -1
+		score := -1000.0
+		for idx, p := range proc {
+			if !available[p.Makes()] {
+				continue
+			}
+			amt := p.Output()
+			area := buckets[p.Makes()].GetArea()
+			if curr := (alloc[area] - currAlloc[area]) * amt; curr > score {
+				best = idx
+				score = curr
+			}
+		}
+		if best < 0 {
+			return
+		}
+
+		labor -= proc[best].Run(exist)
+	}
+}
---
+++ gogames/settlers/economy/production_test.go
@@ -0,0 +1,267 @@
+package production
+
+import (
+	"testing"
+
+	prodpb "gogames/settlers/economy/production_proto"
+)
+
+func TestRequirements(t *testing.T) {
+	cases := []struct {
+		desc  string
+		exist map[string]float64
+		goods []*prodpb.Good
+		want  map[string]bool
+	}{
+		{
+			desc:  "No requirements",
+			exist: map[string]float64{},
+			goods: []*prodpb.Good{
+				{
+					Key: "one",
+				},
+				{
+					Key: "two",
+				},
+			},
+			want: map[string]bool{
+				"one": true, "two": true,
+			},
+		},
+		{
+			desc:  "Require full bucket (fail)",
+			exist: map[string]float64{"one": 0.5},
+			goods: []*prodpb.Good{
+				{
+					Key:     "one",
+					Maximum: 1.0,
+				},
+				{
+					Key: "two",
+					Prereqs: []*prodpb.Requirement{
+						{
+							Key:  "one",
+							Kind: prodpb.Requirement_RK_FULL,
+						},
+					},
+				},
+			},
+			want: map[string]bool{
+				"one": true,
+			},
+		},
+		{
+			desc:  "Require full bucket (succeed)",
+			exist: map[string]float64{"one": 1.0},
+			goods: []*prodpb.Good{
+				{
+					Key:     "one",
+					Maximum: 1.0,
+				},
+				{
+					Key: "two",
+					Prereqs: []*prodpb.Requirement{
+						{
+							Key:  "one",
+							Kind: prodpb.Requirement_RK_FULL,
+						},
+					},
+				},
+			},
+			want: map[string]bool{
+				"one": true, "two": true,
+			},
+		},
+		{
+			desc:  "Require amount (fail)",
+			exist: map[string]float64{"one": 0.5},
+			goods: []*prodpb.Good{
+				{
+					Key: "one",
+				},
+				{
+					Key: "two",
+					Prereqs: []*prodpb.Requirement{
+						{
+							Key:    "one",
+							Kind:   prodpb.Requirement_RK_MIN,
+							Amount: 0.75,
+						},
+					},
+				},
+			},
+			want: map[string]bool{
+				"one": true,
+			},
+		},
+		{
+			desc:  "Require amount (succeed)",
+			exist: map[string]float64{"one": 1.0},
+			goods: []*prodpb.Good{
+				{
+					Key: "one",
+				},
+				{
+					Key: "two",
+					Prereqs: []*prodpb.Requirement{
+						{
+							Key:    "one",
+							Kind:   prodpb.Requirement_RK_MIN,
+							Amount: 0.75,
+						},
+					},
+				},
+			},
+			want: map[string]bool{
+				"one": true, "two": true,
+			},
+		},
+		{
+			desc:  "Require cap (fail)",
+			exist: map[string]float64{"one": 1.0},
+			goods: []*prodpb.Good{
+				{
+					Key: "one",
+				},
+				{
+					Key: "two",
+					Prereqs: []*prodpb.Requirement{
+						{
+							Key:    "one",
+							Kind:   prodpb.Requirement_RK_CAP,
+							Amount: 0.75,
+						},
+					},
+				},
+			},
+			want: map[string]bool{
+				"one": true,
+			},
+		},
+		{
+			desc:  "Require cap (succeed)",
+			exist: map[string]float64{"one": 0.5},
+			goods: []*prodpb.Good{
+				{
+					Key: "one",
+				},
+				{
+					Key: "two",
+					Prereqs: []*prodpb.Requirement{
+						{
+							Key:    "one",
+							Kind:   prodpb.Requirement_RK_CAP,
+							Amount: 0.75,
+						},
+					},
+				},
+			},
+			want: map[string]bool{
+				"one": true, "two": true,
+			},
+		},
+		{
+			desc:  "Combos",
+			exist: map[string]float64{"one": 0.5, "two": 0.25, "three": 0.33, "four": 1.0},
+			goods: []*prodpb.Good{
+				{
+					Key:     "one",
+					Maximum: 1.0,
+					Prereqs: []*prodpb.Requirement{
+						{
+							Key:  "two",
+							Kind: prodpb.Requirement_RK_FULL,
+						},
+						{
+							Key:    "three",
+							Kind:   prodpb.Requirement_RK_MIN,
+							Amount: 0.25,
+						},
+					},
+				},
+				{
+					Key:     "two",
+					Maximum: 1.0,
+					Prereqs: []*prodpb.Requirement{
+						{
+							Key:  "four",
+							Kind: prodpb.Requirement_RK_FULL,
+						},
+						{
+							Key:    "one",
+							Kind:   prodpb.Requirement_RK_MIN,
+							Amount: 0.25,
+						},
+						{
+							Key:    "three",
+							Kind:   prodpb.Requirement_RK_CAP,
+							Amount: 0.5,
+						},
+					},
+				},
+				{
+					Key:     "three",
+					Maximum: 1.0,
+					Prereqs: []*prodpb.Requirement{
+						{
+							Key:  "four",
+							Kind: prodpb.Requirement_RK_FULL,
+						},
+						{
+							Key:    "one",
+							Kind:   prodpb.Requirement_RK_MIN,
+							Amount: 0.25,
+						},
+						{
+							Key:    "two",
+							Kind:   prodpb.Requirement_RK_CAP,
+							Amount: 0.15,
+						},
+					},
+				},
+				{
+					Key:     "four",
+					Maximum: 1.0,
+					Prereqs: []*prodpb.Requirement{
+						{
+							Key:  "one",
+							Kind: prodpb.Requirement_RK_FULL,
+						},
+						{
+							Key:    "two",
+							Kind:   prodpb.Requirement_RK_MIN,
+							Amount: 0.25,
+						},
+						{
+							Key:    "three",
+							Kind:   prodpb.Requirement_RK_CAP,
+							Amount: 0.50,
+						},
+					},
+				},
+			},
+			want: map[string]bool{
+				"two": true,
+			},
+		},
+	}
+
+	for _, cc := range cases {
+		t.Run(cc.desc, func(t *testing.T) {
+			buckets := make(map[string]*prodpb.Good)
+			for _, g := range cc.goods {
+				buckets[g.GetKey()] = g
+			}
+			got := filterGoods(cc.exist, buckets)
+			for key := range got {
+				if !cc.want[key] {
+					t.Errorf("%s: filterGoods() => %q, not wanted.", cc.desc, key)
+				}
+				delete(cc.want, key)
+			}
+			if len(cc.want) > 0 {
+				t.Errorf("%s: filterGoods() => %v, missing %v", cc.desc, got, cc.want)
+			}
+		})
+	}
+}
