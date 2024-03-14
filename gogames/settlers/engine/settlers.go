6566293b5df8627997147b1b0af20682140a643d
kingofmen <rolf.andreassen@gmail.com>
2024-03-13 23:16:28 -0700

    Add DNA id string to faction.

--- gogames/settlers/engine/settlers.go
+++ gogames/settlers/engine/settlers.go
@@ -1,10 +1,30 @@
 // Package settlers defines the core gameplay loop.
 package settlers
 
+import (
+	"gogames/util/dna"
+)
+
 // Faction models a tribe or nation.
 type Faction struct {
 	// Number of people not assigned to a specific settlement.
 	Freemen int64
+
+	// uuid is the unique identifying string of the faction.
+	uuid dna.Sequence
+}
+
+func NewFaction(id string) *Faction {
+	return &Faction{
+		uuid: dna.FromString(id),
+	}
+}
+
+func (f *Faction) DNA() dna.Sequence {
+	if f == nil {
+		return dna.FromString("00000000")
+	}
+	return f.uuid
 }
 
 // GameState holds the state of a specific game.
