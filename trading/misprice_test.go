17efc385f2a7412497d80ca9de1b73b0c09fa47f
kingofmen <rolf.andreassen@gmail.com>
2024-06-22 12:17:03 -0700

    Nontrivial test for createPairs.

--- trading/misprice_test.go
+++ trading/misprice_test.go
@@ -3,14 +3,16 @@ package misprice
 import (
 	//"fmt"
 	"testing"
-	//"time"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"trading/marketdata"
 	"trading/polygon"
 )
 
 func TestCreatePairs(t *testing.T) {
+	utc := time.FixedZone("UTC", 0)
 	cases := []struct {
 		desc  string
 		input map[*polygon.Ticker]*marketdata.OptionQuote
@@ -21,12 +23,66 @@ func TestCreatePairs(t *testing.T) {
 			input: map[*polygon.Ticker]*marketdata.OptionQuote{},
 			want:  []*pair{},
 		},
+		{
+			desc: "Creates one pair",
+			input: map[*polygon.Ticker]*marketdata.OptionQuote{
+				&polygon.Ticker{
+					ContractType: "call",
+					ExpDate:      "2024-01-01",
+					Price:        100,
+				}: &marketdata.OptionQuote{
+					AskPrice: []float64{1},
+					AskSize:  []float64{2},
+					BidPrice: []float64{0.5},
+					BidSize:  []float64{3},
+				},
+				&polygon.Ticker{
+					ContractType: "put",
+					ExpDate:      "2024-01-01",
+					Price:        100,
+				}: &marketdata.OptionQuote{
+					AskPrice: []float64{1.3},
+					AskSize:  []float64{4},
+					BidPrice: []float64{0.75},
+					BidSize:  []float64{33},
+				},
+			},
+			want: []*pair{
+				{
+					strike:  100,
+					expire:  time.Date(2024, time.January, 1, 0, 0, 0, 0, utc),
+					putAsk:  price{1.3, 4},
+					putBid:  price{0.75, 33},
+					callAsk: price{1, 2},
+					callBid: price{0.5, 3},
+				},
+			},
+		},
+	}
+
+	cmpOpts := []cmp.Option{
+		cmp.AllowUnexported(pair{}, price{}),
+		cmpopts.SortSlices(func(one, two *pair) bool {
+			if one.expire.Before(two.expire) {
+				return true
+			}
+			if one.strike < two.strike {
+				return true
+			}
+			if one.putAsk.money < two.putAsk.money {
+				return true
+			}
+			if one.callAsk.money < two.callAsk.money {
+				return true
+			}
+			return false
+		}),
 	}
 
 	for _, cc := range cases {
 		t.Run(cc.desc, func(t *testing.T) {
 			got := createPairs(cc.input)
-			if diff := cmp.Diff(cc.want, got); diff != "" {
+			if diff := cmp.Diff(cc.want, got, cmpOpts...); diff != "" {
 				t.Errorf("%s: createPairs() => %+v, want %+v, diff %s", cc.desc, got, cc.want, diff)
 			}
 		})
