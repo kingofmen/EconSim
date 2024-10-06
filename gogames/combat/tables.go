7547b9d4f2c9a581e785b06b0e859ae143b9492e
kingofmen <rolf.andreassen@gmail.com>
2024-10-05 23:32:00 -0700

    All-features test for table Resolve logic.

--- gogames/combat/tables.go
+++ gogames/combat/tables.go
@@ -10,6 +10,12 @@ import (
 	tpb "gogames/combat/tables_go_proto"
 )
 
+// DieRoller encapsulates a random-number source.
+type DieRoller interface {
+	Roll(face int32) int32
+}
+
+// Lookup gives the result of a set of Phases.
 type Lookup struct {
 	states map[string]*tpb.Phase
 }
@@ -40,6 +46,9 @@ func (l *Lookup) AddPhases(phases []*tpb.Phase) []error {
 			errors = append(errors, fmt.Errorf("duplicate key %q in phase %d", key, idx))
 			continue
 		}
+		if len(ph.GetRolls()) < 1 {
+			errors = append(errors, fmt.Errorf("phase %q has no dierolls", key))
+		}
 		l.states[key] = ph
 	}
 
@@ -58,6 +67,9 @@ func (l *Lookup) AddPhases(phases []*tpb.Phase) []error {
 					errors = append(errors, fmt.Errorf("result %q in phase %q both ends and goes to %q", name, key, next))
 				}
 			}
+			if p := res.GetPips(); p < 1 {
+				errors = append(errors, fmt.Errorf("result %q in phase %q has fewer pips than 1", name, key))
+			}
 			if len(next) > 0 {
 				if _, exists := l.states[next]; !exists {
 					errors = append(errors, fmt.Errorf("result %q in phase %q goes to nonexistent %q", name, key, next))
@@ -72,10 +84,142 @@ func (l *Lookup) AddPhases(phases []*tpb.Phase) []error {
 	return errors
 }
 
-func (l *Lookup) Resolve(key string, lookup logic.Lookup) (map[string]int32, error) {
-	_, ok := l.states[key]
+// rollDice returns the unmodified result of rolling the dice for the phase.
+func rollDice(phase *tpb.Phase, dice DieRoller) int32 {
+	var roll int32
+	for _, r := range phase.GetRolls() {
+		roll += dice.Roll(r)
+	}
+	return roll
+}
+
+// getModifiers returns the sum of the applicable modifiers for the phease.
+func getModifiers(phase *tpb.Phase, lookup logic.Lookup) ([]*tpb.Modifier, int32, error) {
+	var mods []*tpb.Modifier
+	var value int32
+	for _, mod := range phase.GetModifiers() {
+		allow := true
+		for idx, req := range mod.GetRequires() {
+			ok, err := logic.Eval(req, lookup)
+			if err != nil {
+				return nil, 0, fmt.Errorf("error evaluating requirement %d of modifier %q in phase %q: %w", idx, mod.GetKey(), phase.GetKey(), err)
+			}
+			if ok {
+				continue
+			}
+			allow = false
+			break
+		}
+		if !allow {
+			continue
+		}
+		mods = append(mods, mod)
+		value += mod.GetValue()
+	}
+	return mods, value, nil
+}
+
+// getOutcome returns the phase result indicated by the roll.
+func getOutcome(roll int32, phase *tpb.Phase, lookup logic.Lookup) (*tpb.Result, error) {
+	var accum int32
+	var outcome *tpb.Result
+	for _, result := range phase.GetResults() {
+		allow := true
+		for idx, req := range result.GetRequires() {
+			ok, err := logic.Eval(req, lookup)
+			if err != nil {
+				return nil, fmt.Errorf("error evaluating requirement %d of result %q in phase %q: %w", idx, result.GetKey(), phase.GetKey(), err)
+			}
+			if ok {
+				continue
+			}
+			allow = false
+			break
+		}
+		if !allow {
+			continue
+		}
+		if accum += result.GetPips(); accum < roll {
+			continue
+		}
+		outcome = result
+		break
+	}
+	if accum < 1 {
+		return nil, fmt.Errorf("phase %q has %d results none of which are allowed", phase.GetKey(), len(phase.GetResults()))
+	}
+	return outcome, nil
+}
+
+func (l *Lookup) Resolve(key string, dice DieRoller, lookup logic.Lookup) (*tpb.Outcome, error) {
+	phase, ok := l.states[key]
 	if !ok {
 		return nil, fmt.Errorf("unknown starting state %q", key)
 	}
-	return nil, nil
+
+	maxIters := 10000
+	var iter int
+	fullResult := &tpb.Outcome{
+		Flags: make(map[string]int32),
+	}
+	for {
+		pName := phase.GetKey()
+		if iter++; iter > maxIters {
+			return nil, fmt.Errorf("table entry %q not resolved after %d iterations; final phase was %q", key, iter, pName)
+		}
+		roll := rollDice(phase, dice)
+		mods, add, err := getModifiers(phase, lookup)
+		if err != nil {
+			return nil, err
+		}
+		outcome, err := getOutcome(roll+add, phase, lookup)
+		if err != nil {
+			return nil, err
+		}
+		report := &tpb.Result{
+			Key:  outcome.GetKey(),
+			Pips: roll,
+		}
+		fullResult.Phases = append(fullResult.Phases, &tpb.Phase{
+			Key:       pName,
+			Modifiers: make([]*tpb.Modifier, 0, len(mods)),
+			Results:   []*tpb.Result{report},
+		})
+		for _, mod := range mods {
+			report.Flags = append(report.Flags, &tpb.Modifier{
+				Key:   mod.GetKey(),
+				Value: mod.GetValue(),
+			})
+		}
+		if outcome == nil {
+			continue
+		}
+		for idx, f := range outcome.GetFlags() {
+			apply := true
+			for _, req := range f.GetRequires() {
+				match, err := logic.Eval(req, lookup)
+				if err != nil {
+					return nil, fmt.Errorf("error evaluating flag %d in outcome %q of phase %q: %w", idx, outcome.GetKey(), phase.GetKey(), err)
+				}
+				if !match {
+					apply = false
+					break
+				}
+			}
+			if !apply {
+				continue
+			}
+			fullResult.Flags[f.GetKey()] += f.GetValue()
+		}
+
+		if outcome.GetEnds() {
+			break
+		}
+		phase, ok = l.states[outcome.GetGoto()]
+		if !ok {
+			return nil, fmt.Errorf("bad next-phase %q from phase %q", outcome.GetGoto(), pName)
+		}
+	}
+
+	return fullResult, nil
 }
--- gogames/combat/tables_test.go
+++ gogames/combat/tables_test.go
@@ -4,12 +4,13 @@ import (
 	"strings"
 	"testing"
 
-	//"github.com/google/go-cmp/cmp"
-	//"google.golang.org/protobuf/encoding/prototext"
-	//"google.golang.org/protobuf/testing/protocmp"
+	"github.com/google/go-cmp/cmp"
+	"gogames/util/logic"
+	"google.golang.org/protobuf/encoding/prototext"
+	"google.golang.org/protobuf/testing/protocmp"
 
 	tpb "gogames/combat/tables_go_proto"
-	//lpb "gogames/util/logic_go_proto"
+	lpb "gogames/util/logic_go_proto"
 )
 
 func TestValidation(t *testing.T) {
@@ -27,19 +28,44 @@ func TestValidation(t *testing.T) {
 			desc: "Trivial case",
 			phases: []*tpb.Phase{
 				&tpb.Phase{
-					Key: "start_and_finish",
+					Key:   "start_and_finish",
+					Rolls: []int32{6},
 					Results: []*tpb.Result{
-						&tpb.Result{Ends: true},
+						&tpb.Result{
+							Ends: true,
+							Pips: 1,
+						},
 					},
 				},
 			},
 			lookup: NewLookup(),
 		},
 		{
+			desc: "Missing pips",
+			phases: []*tpb.Phase{
+				&tpb.Phase{
+					Key:   "start_and_finish",
+					Rolls: []int32{6},
+					Results: []*tpb.Result{
+						&tpb.Result{
+							Ends: true,
+						},
+					},
+				},
+			},
+			lookup: NewLookup(),
+			errors: []string{"fewer pips than 1"},
+		},
+		{
 			desc: "Empty key",
 			phases: []*tpb.Phase{
 				&tpb.Phase{
-					Results: []*tpb.Result{},
+					Rolls: []int32{6},
+					Results: []*tpb.Result{
+						&tpb.Result{
+							Pips: 1,
+						},
+					},
 				},
 			},
 			lookup: NewLookup(),
@@ -49,15 +75,22 @@ func TestValidation(t *testing.T) {
 			desc: "Duplicate key",
 			phases: []*tpb.Phase{
 				&tpb.Phase{
-					Key: "start_and_finish",
+					Key:   "start_and_finish",
+					Rolls: []int32{6},
 					Results: []*tpb.Result{
-						&tpb.Result{Ends: true},
+						&tpb.Result{
+							Ends: true,
+							Pips: 1,
+						},
 					},
 				},
 				&tpb.Phase{
 					Key: "start_and_finish",
 					Results: []*tpb.Result{
-						&tpb.Result{Ends: true},
+						&tpb.Result{
+							Ends: true,
+							Pips: 1,
+						},
 					},
 				},
 			},
@@ -65,21 +98,46 @@ func TestValidation(t *testing.T) {
 			errors: []string{"duplicate key"},
 		},
 		{
+			desc: "No rolls",
+			phases: []*tpb.Phase{
+				&tpb.Phase{
+					Key: "start_and_finish",
+					Results: []*tpb.Result{
+						&tpb.Result{
+							Ends: true,
+							Pips: 1,
+						},
+					},
+				},
+			},
+			lookup: NewLookup(),
+			errors: []string{"has no dierolls"},
+		},
+		{
 			desc: "No results",
 			phases: []*tpb.Phase{
 				&tpb.Phase{
-					Key: "start",
+					Key:   "start",
+					Rolls: []int32{6},
 					Results: []*tpb.Result{
-						&tpb.Result{Goto: "middle"},
+						&tpb.Result{
+							Goto: "middle",
+							Pips: 1,
+						},
 					},
 				},
 				&tpb.Phase{
-					Key: "middle",
+					Key:   "middle",
+					Rolls: []int32{6},
 				},
 				&tpb.Phase{
-					Key: "finish",
+					Key:   "finish",
+					Rolls: []int32{6},
 					Results: []*tpb.Result{
-						&tpb.Result{Ends: true},
+						&tpb.Result{
+							Ends: true,
+							Pips: 1,
+						},
 					},
 				},
 			},
@@ -90,21 +148,34 @@ func TestValidation(t *testing.T) {
 			desc: "End and next",
 			phases: []*tpb.Phase{
 				&tpb.Phase{
-					Key: "start",
+					Key:   "start",
+					Rolls: []int32{6},
 					Results: []*tpb.Result{
-						&tpb.Result{Goto: "middle", Ends: true},
+						&tpb.Result{
+							Goto: "middle",
+							Ends: true,
+							Pips: 1,
+						},
 					},
 				},
 				&tpb.Phase{
-					Key: "middle",
+					Key:   "middle",
+					Rolls: []int32{6},
 					Results: []*tpb.Result{
-						&tpb.Result{Goto: "finish"},
+						&tpb.Result{
+							Goto: "finish",
+							Pips: 1,
+						},
 					},
 				},
 				&tpb.Phase{
-					Key: "finish",
+					Key:   "finish",
+					Rolls: []int32{6},
 					Results: []*tpb.Result{
-						&tpb.Result{Ends: true},
+						&tpb.Result{
+							Ends: true,
+							Pips: 1,
+						},
 					},
 				},
 			},
@@ -115,21 +186,33 @@ func TestValidation(t *testing.T) {
 			desc: "Goes to unknown phase",
 			phases: []*tpb.Phase{
 				&tpb.Phase{
-					Key: "start",
+					Key:   "start",
+					Rolls: []int32{6},
 					Results: []*tpb.Result{
-						&tpb.Result{Goto: "whatever"},
+						&tpb.Result{
+							Goto: "whatever",
+							Pips: 1,
+						},
 					},
 				},
 				&tpb.Phase{
-					Key: "middle",
+					Key:   "middle",
+					Rolls: []int32{6},
 					Results: []*tpb.Result{
-						&tpb.Result{Goto: "finish"},
+						&tpb.Result{
+							Goto: "finish",
+							Pips: 1,
+						},
 					},
 				},
 				&tpb.Phase{
-					Key: "finish",
+					Key:   "finish",
+					Rolls: []int32{6},
 					Results: []*tpb.Result{
-						&tpb.Result{Ends: true},
+						&tpb.Result{
+							Ends: true,
+							Pips: 1,
+						},
 					},
 				},
 			},
@@ -140,21 +223,33 @@ func TestValidation(t *testing.T) {
 			desc: "Happy case",
 			phases: []*tpb.Phase{
 				&tpb.Phase{
-					Key: "start",
+					Key:   "start",
+					Rolls: []int32{6},
 					Results: []*tpb.Result{
-						&tpb.Result{Goto: "middle"},
+						&tpb.Result{
+							Goto: "middle",
+							Pips: 1,
+						},
 					},
 				},
 				&tpb.Phase{
-					Key: "middle",
+					Key:   "middle",
+					Rolls: []int32{6},
 					Results: []*tpb.Result{
-						&tpb.Result{Goto: "finish"},
+						&tpb.Result{
+							Goto: "finish",
+							Pips: 1,
+						},
 					},
 				},
 				&tpb.Phase{
-					Key: "finish",
+					Key:   "finish",
+					Rolls: []int32{6},
 					Results: []*tpb.Result{
-						&tpb.Result{Ends: true},
+						&tpb.Result{
+							Ends: true,
+							Pips: 1,
+						},
 					},
 				},
 			},
@@ -175,4 +270,227 @@ func TestValidation(t *testing.T) {
 			}
 		})
 	}
+}
+
+// testDice provides a predetermined series of rolls;
+// it implements DieRoller.
+type testDice struct {
+	idx   int
+	rolls []int32
+}
+
+func (td *testDice) Roll(face int32) int32 {
+	td.idx++
+	if nr := len(td.rolls) - 1; nr < td.idx {
+		return td.rolls[nr]
+	}
+	return td.rolls[td.idx-1]
+}
+
+func TestResolve(t *testing.T) {
+	oneGTtwo := &lpb.Predicate{
+		Test: &lpb.Predicate_Comp{
+			Comp: &lpb.Compare{
+				KeyOne:    "1",
+				KeyTwo:    "2",
+				Operation: lpb.Compare_CMP_GT,
+			},
+		},
+	}
+	twoGTone := &lpb.Predicate{
+		Test: &lpb.Predicate_Comp{
+			Comp: &lpb.Compare{
+				KeyOne:    "2",
+				KeyTwo:    "1",
+				Operation: lpb.Compare_CMP_GT,
+			},
+		},
+	}
+
+	cases := []struct {
+		desc     string
+		dice     *testDice
+		phases   []*tpb.Phase
+		llookup  *logic.TestLookup
+		startKey string
+		want     *tpb.Outcome
+	}{
+		{
+			desc: "Exercise all features",
+			dice: &testDice{
+				rolls: []int32{1, 2, 1, 2, 1, 2},
+			},
+			phases: []*tpb.Phase{
+				&tpb.Phase{
+					Key:   "start",
+					Rolls: []int32{6},
+					Results: []*tpb.Result{
+						&tpb.Result{
+							Key:  "start_reroll",
+							Pips: int32(2),
+							Goto: "start",
+						},
+						&tpb.Result{
+							Key:  "start_next",
+							Pips: int32(1),
+							Goto: "middle",
+						},
+					},
+					Modifiers: []*tpb.Modifier{
+						&tpb.Modifier{
+							Key:      "start_plus_one",
+							Value:    int32(1),
+							Requires: []*lpb.Predicate{twoGTone},
+						},
+					},
+				},
+				&tpb.Phase{
+					Key:   "middle",
+					Rolls: []int32{6},
+					Results: []*tpb.Result{
+						&tpb.Result{
+							Key:  "middle_reroll",
+							Pips: int32(1),
+							Goto: "middle",
+						},
+						&tpb.Result{
+							Key:  "middle_next",
+							Pips: int32(1),
+							Goto: "finish",
+						},
+					},
+					Modifiers: []*tpb.Modifier{
+						&tpb.Modifier{
+							Key:      "middle_minus_one",
+							Value:    int32(-11),
+							Requires: []*lpb.Predicate{oneGTtwo},
+						},
+					},
+				},
+				&tpb.Phase{
+					Key:   "finish",
+					Rolls: []int32{6},
+					Results: []*tpb.Result{
+						&tpb.Result{
+							Key:  "finish_reroll",
+							Pips: int32(1),
+							Goto: "finish",
+							Flags: []*tpb.Modifier{
+								&tpb.Modifier{
+									Key:      "set_this_flag",
+									Value:    1,
+									Requires: []*lpb.Predicate{twoGTone},
+								},
+								&tpb.Modifier{
+									Key:      "not_this_flag",
+									Value:    1,
+									Requires: []*lpb.Predicate{oneGTtwo},
+								},
+							},
+						},
+						&tpb.Result{
+							Key:  "end",
+							Pips: int32(1),
+							Ends: true,
+						},
+					},
+				},
+			},
+			llookup:  logic.NewTestLookup(),
+			startKey: "start",
+			want: &tpb.Outcome{
+				Flags: map[string]int32{
+					"set_this_flag": 1,
+				},
+				Phases: []*tpb.Phase{
+					&tpb.Phase{
+						Key: "start",
+						Results: []*tpb.Result{
+							&tpb.Result{
+								Key:  "start_reroll",
+								Pips: 1,
+								Flags: []*tpb.Modifier{
+									&tpb.Modifier{
+										Key:   "start_plus_one",
+										Value: int32(1),
+									},
+								},
+							},
+						},
+					},
+					&tpb.Phase{
+						Key: "start",
+						Results: []*tpb.Result{
+							&tpb.Result{
+								Key:  "start_next",
+								Pips: 2,
+								Flags: []*tpb.Modifier{
+									&tpb.Modifier{
+										Key:   "start_plus_one",
+										Value: int32(1),
+									},
+								},
+							},
+						},
+					},
+					&tpb.Phase{
+						Key: "middle",
+						Results: []*tpb.Result{
+							&tpb.Result{
+								Key:  "middle_reroll",
+								Pips: 1,
+							},
+						},
+					},
+					&tpb.Phase{
+						Key: "middle",
+						Results: []*tpb.Result{
+							&tpb.Result{
+								Key:  "middle_next",
+								Pips: 2,
+							},
+						},
+					},
+					&tpb.Phase{
+						Key: "finish",
+						Results: []*tpb.Result{
+							&tpb.Result{
+								Key:  "finish_reroll",
+								Pips: 1,
+							},
+						},
+					},
+					&tpb.Phase{
+						Key: "finish",
+						Results: []*tpb.Result{
+							&tpb.Result{
+								Key:  "end",
+								Pips: 2,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, cc := range cases {
+		t.Run(cc.desc, func(t *testing.T) {
+			lookup := NewLookup()
+			if errs := lookup.AddPhases(cc.phases); len(errs) > 0 {
+				t.Fatalf("%s: AddPhases() => %v, want nil", cc.desc, errs)
+			}
+			outcome, err := lookup.Resolve(cc.startKey, cc.dice, cc.llookup)
+			if err != nil {
+				t.Errorf("%s: Resolve() => %v, want nil", cc.desc, err)
+			}
+			if diff := cmp.Diff(outcome, cc.want, protocmp.Transform()); len(diff) > 0 {
+				t.Errorf("%s: Resolve() => %s, want %s, diff %s", cc.desc, prototext.Format(outcome), prototext.Format(cc.want), diff)
+			}
+		})
+	}
+}
+
+func TestResolveErrors(t *testing.T) {
+	// TODO
 }
