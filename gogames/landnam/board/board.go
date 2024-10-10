62a4b40a8f0229cd0512977fbff236ddb4fa92f9
kingofmen <rolf.andreassen@gmail.com>
2024-10-09 23:00:05 -0700

    Add pops to tile proto, clean up imports.

--- gogames/landnam/board/board.go
+++ gogames/landnam/board/board.go
@@ -1,20 +1,27 @@
 package board
 
 import (
-	//"fmt"
+	"fmt"
 
-	//"gogames/util/logic"
 	"gogames/tiles/triangles"
 
 	brdpb "gogames/landnam/board/board_go_proto"
 )
 
-// ValidateTiles checks that the tiles have allowed tri-coordinates.
+const (
+	kMaxPops = 10
+)
+
+// ValidateTiles checks that the tiles are in allowed states.
 func ValidateTiles(brd *brdpb.Board) []error {
 	errs := make([]error, 0, len(brd.GetTiles()))
 	for _, tile := range brd.GetTiles() {
-		if err := triangles.ProtoValid(tile.GetCoords()); err != nil {
+		tp := tile.GetCoords()
+		if err := triangles.ProtoValid(tp); err != nil {
 			errs = append(errs, err)
+		}
+		if np := len(tile.GetPops()); np > kMaxPops {
+			errs = append(errs, fmt.Errorf("tile %s has %d pops, max is %d", triangles.ProtoString(tp), np, kMaxPops))
 		}
 	}
 	return errs
--- gogames/landnam/board/board_test.go
+++ gogames/landnam/board/board_test.go
@@ -9,6 +9,7 @@ import (
 	//"google.golang.org/protobuf/testing/protocmp"
 
 	brdpb "gogames/landnam/board/board_go_proto"
+	poppb "gogames/landnam/population/pop_go_proto"
 	tripb "gogames/tiles/triangles/triangles_go_proto"
 	//lpb "gogames/util/logic_go_proto"
 )
@@ -20,7 +21,7 @@ func TestValidation(t *testing.T) {
 		want  []string
 	}{
 		{
-			desc: "Happy case",
+			desc: "Good triangles",
 			board: &brdpb.Board{
 				Tiles: []*brdpb.Tile{
 					&brdpb.Tile{Coords: &tripb.TriPoint{A: 1, B: 1, C: -1}},
@@ -37,6 +38,30 @@ func TestValidation(t *testing.T) {
 				},
 			},
 			want: []string{"invalid triangle coordinates"},
+		},
+		{
+			desc: "Bad pops",
+			board: &brdpb.Board{
+				Tiles: []*brdpb.Tile{
+					&brdpb.Tile{
+						Coords: &tripb.TriPoint{A: 1, B: 1, C: -1},
+						Pops: []*poppb.Pop{
+							&poppb.Pop{},
+							&poppb.Pop{},
+							&poppb.Pop{},
+							&poppb.Pop{},
+							&poppb.Pop{},
+							&poppb.Pop{},
+							&poppb.Pop{},
+							&poppb.Pop{},
+							&poppb.Pop{},
+							&poppb.Pop{},
+							&poppb.Pop{},
+						},
+					},
+				},
+			},
+			want: []string{"pops, max is"},
 		},
 	}
 
