b9129736a1a376fb2b45adaa36d615054ca4857f
kingofmen <rolf.andreassen@gmail.com>
2024-10-10 19:26:59 -0700

    Stub game loop for landnam.

--- gogames/landnam/core/game.go
+++ gogames/landnam/core/game.go
@@ -2,12 +2,49 @@
 package game
 
 import (
+	"fmt"
+
+	"gogames/landnam/population/pop"
 	"google.golang.org/protobuf/proto"
 
 	gmpb "gogames/landnam/core/game_go_proto"
+	poppb "gogames/landnam/population/pop_go_proto"
 )
 
-func Turn(oldState *gmpb.GameState) *gmpb.GameState {
+type Engine struct {
+	popMgr *pop.Manager
+}
+
+func (e *Engine) Turn(oldState *gmpb.GameState) (*gmpb.GameState, error) {
 	newState := proto.Clone(oldState).(*gmpb.GameState)
-	return newState
+	board := newState.GetBoard()
+	pops := make([]*poppb.Pop, 0, len(board.GetTiles())*10)
+	for _, tile := range board.GetTiles() {
+		for _, pop := range tile.GetPops() {
+			pops = append(pops, pop)
+		}
+	}
+
+	trades := make(map[*poppb.Pop][]*poppb.Pop)
+	// TODO: Trade goes here.
+
+	if err := e.popMgr.Produce(pops, trades); err != nil {
+		return oldState, fmt.Errorf("unable to run production: %w", err)
+	}
+
+	// TODO: Raids and battles go here.
+
+	if err := e.popMgr.Consume(pops); err != nil {
+		return oldState, fmt.Errorf("unable to run consumption: %w", err)
+	}
+	if err := e.popMgr.Demographics(pops); err != nil {
+		return oldState, fmt.Errorf("unable to run demographics: %w", err)
+	}
+	if err := e.popMgr.Evolve(pops); err != nil {
+		return oldState, fmt.Errorf("unable to run updates: %w", err)
+	}
+
+	// TODO: Scoring goes here.
+
+	return newState, nil
 }
