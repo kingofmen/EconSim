415ad2f7a8a19c092e0f7b7b77c49edf531cd109
kingofmen <rolf.andreassen@gmail.com>
2024-10-13 18:53:14 -0700

    Stub handleCampaign method.

--- gogames/landnam/core/game.go
+++ gogames/landnam/core/game.go
@@ -15,32 +15,42 @@ type Engine struct {
 	popMgr *pop.Manager
 }
 
+func (e *Engine) handleCampaign(cmp *gmpb.Campaign, popMap map[string]*poppb.Pop) error {
+	return nil
+}
+
 func (e *Engine) Turn(oldState *gmpb.GameState) (*gmpb.GameState, error) {
 	newState := proto.Clone(oldState).(*gmpb.GameState)
 	board := newState.GetBoard()
-	pops := make([]*poppb.Pop, 0, len(board.GetTiles())*10)
+	popList := make([]*poppb.Pop, 0, len(board.GetTiles())*10)
+	popMap := make(map[string]*poppb.Pop)
 	for _, tile := range board.GetTiles() {
 		for _, pop := range tile.GetPops() {
-			pops = append(pops, pop)
+			popList = append(popList, pop)
+			popMap[pop.GetKey()] = pop
 		}
 	}
 
 	trades := make(map[*poppb.Pop][]*poppb.Pop)
 	// TODO: Trade goes here.
 
-	if err := e.popMgr.Produce(pops, trades); err != nil {
+	if err := e.popMgr.Produce(popList, trades); err != nil {
 		return oldState, fmt.Errorf("unable to run production: %w", err)
 	}
 
-	// TODO: Raids and battles go here.
+	for _, cmp := range newState.GetCampaigns() {
+		if err := e.handleCampaign(cmp, popMap); err != nil {
+			return oldState, fmt.Errorf("error handling campaign: %w", err)
+		}
+	}
 
-	if err := e.popMgr.Consume(pops); err != nil {
+	if err := e.popMgr.Consume(popList); err != nil {
 		return oldState, fmt.Errorf("unable to run consumption: %w", err)
 	}
-	if err := e.popMgr.Demographics(pops); err != nil {
+	if err := e.popMgr.Demographics(popList); err != nil {
 		return oldState, fmt.Errorf("unable to run demographics: %w", err)
 	}
-	if err := e.popMgr.Evolve(pops); err != nil {
+	if err := e.popMgr.Evolve(popList); err != nil {
 		return oldState, fmt.Errorf("unable to run updates: %w", err)
 	}
 
